2baef6e16b59f9c13b54bc219435158526381246
Jorge Mimica <jmimica@gmail.com>
2016-10-19 18:48:36 -0200

    Update LeitorXML.java
    Added compilation error

--- src/main/java/br/com/caelum/argentum/reader/LeitorXML.java
+++ src/main/java/br/com/caelum/argentum/reader/LeitorXML.java
@@ -13,7 +13,7 @@ public class LeitorXML {
 	@SuppressWarnings("unchecked")
 	public List<Negociacao> carrega(InputStream inputStream) {
 		XStream stream = new XStream(new DomDriver());
-		stream.alias("negociacao", Negociacao.class);
+		stream_MIMICA.alias("negociacao", Negociacao.class);
 		return (List<Negociacao>) stream.fromXML(inputStream);
 	}
 
